fe8c28feb89dc30b0c93658f3b9ca6348cdb2853
dkasprzak <dominik.kasprzak1408@gmail.com>
2024-06-26 18:28:12 +0200

    Create database add some tables

---
+++ eBeauty.Database/EBeauty.SqlDatabase/EBeauty.SqlDatabase/dbo/Tables/Accounts.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Accounts]
+(
+	[Id] INT IDENTITY(1, 1) NOT NULL,
+	[Name] NVARCHAR(100) NOT NULL,
+	[AccountType] NVARCHAR(50) NOT NULL,
+	[CreateDate] DATETIMEOFFSET NOT NULL,
+	[BusinessId] INT,
+	CONSTRAINT [PK_Accounts] PRIMARY KEY CLUSTERED ([Id] ASC),
+);
