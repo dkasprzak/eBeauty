204c1f995125702105e369c0decf906b7ec061a8
dkasprzak <dominik.kasprzak1408@gmail.com>
2024-06-28 16:44:57 +0200

    Add Serilog. Changes in appsetings. Fix swagger schemas IDs

--- eBeauty.Backend/EBeauty.WebApi/Program.cs
+++ eBeauty.Backend/EBeauty.WebApi/Program.cs
@@ -1,13 +1,46 @@
+using Serilog;
+
+const string APP_NAME = "EBeauty.WebApi";
+
+Log.Logger = new LoggerConfiguration()
+    .Enrich.WithProperty("Application", APP_NAME)
+    .Enrich.WithProperty("MachineName", Environment.MachineName)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+if (builder.Environment.IsDevelopment())
+{
+    builder.Configuration.AddJsonFile("appsettings.Development.local.json");
+}
+
+builder.Host.UseSerilog((context, services, configuration) => configuration
+    .Enrich.WithProperty("Application", APP_NAME)
+    .Enrich.WithProperty("MachineName", Environment.MachineName)
+    .ReadFrom.Configuration(context.Configuration)
+    .ReadFrom.Services(services)
+    .Enrich.FromLogContext());
+
+builder.Services.AddControllers();
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(o =>
+{
+    o.CustomSchemaIds(x =>
+    {
+        var name = x.FullName;
+        if (name != null)
+        {
+            name = name.Replace("+", "_");
+        }
+        return name;
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -15,4 +48,5 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.MapControllers();
 app.Run();
