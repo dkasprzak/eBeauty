1b270726baba06e1b34ff4fa571e96965c4df4b9
dkasprzak <dominik.kasprzak1408@gmail.com>
2024-06-29 13:20:00 +0200

    Add SQL script to create OpeningHours table

---
+++ eBeauty.Database/EBeauty.SqlDatabase/EBeauty.SqlDatabase/dbo/Tables/OpeningHours.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[OpeningHours]
+(
+	[Id] INT IDENTITY(1, 1) NOT NULL,
+	[DayOfWeek] VARCHAR(20) NOT NULL,
+	[OpeningTime] TIME,
+	[ClosingTime] TIME,
+	[BusinessId] INT NOT NULL,
+	CONSTRAINT [PK_OpeningHours] PRIMARY KEY CLUSTERED ([Id] ASC),
+	CONSTRAINT [FK_OpeningHours_Businesses] FOREIGN KEY ([BusinessId]) REFERENCES [dbo].[Businesses] ([Id]) ON DELETE CASCADE
+);
+
+GO
+CREATE INDEX [OQ_OpeningHours_DayOfWeek] ON [dbo].[OpeningHours] ([DayOfWeek]);
+
+GO 
+CREATE INDEX [IX_OpeningHours_BusinessId] ON [dbo].[OpeningHours] ([BusinessId]);
