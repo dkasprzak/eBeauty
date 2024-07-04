306a708cc28a596de7f3f04570e469ddb984bc2a
dkasprzak <dominik.kasprzak1408@gmail.com>
2024-07-04 11:01:48 +0200

    Current account data

--- eBeauty.Backend/EBeauty.Application/Logic/AccountFunctions/Queries/CurrentAccountQuery.cs
+++ eBeauty.Backend/EBeauty.Application/Logic/AccountFunctions/Queries/CurrentAccountQuery.cs
@@ -1,6 +1,38 @@
-﻿namespace EBeauty.Application.Logic.AccountFunctions.Queries;
+﻿using EBeauty.Application.Exceptions;
+using EBeauty.Application.Interfaces;
+using EBeauty.Application.Logic.Abstractions;
+using EFCoreSecondLevelCacheInterceptor;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace EBeauty.Application.Logic.AccountFunctions.Queries;
 
 public class CurrentAccountQuery
 {
-    
+    public record Request : IRequest<Result>
+    {
+    }
+
+    public record Result
+    {
+        public required string Name { get; set; }
+    }
+
+    public class Handler : BaseQueryHandler, IRequestHandler<Request, Result>
+    {
+
+        public Handler(ICurrentAccountProvider currentAccountProvider, IApplicationDbContext applicationDbContext) : base(currentAccountProvider, applicationDbContext)
+        {
+        }
+
+        public async Task<Result> Handle(Request request, CancellationToken cancellationToken)
+        {
+            var account = await _currentAccountProvider.GetAuthenticatedAccount();
+
+            return new Result
+            {
+                Name = account.Name
+            };
+        }
+    } 
 }
--- eBeauty.Backend/EBeauty.WebApi/Controllers/AccountController.cs
+++ eBeauty.Backend/EBeauty.WebApi/Controllers/AccountController.cs
@@ -1,6 +1,21 @@
-﻿namespace EBeauty.WebApi.Controllers;
+﻿using EBeauty.Application.Logic.AccountFunctions.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
-public class AccountController
+namespace EBeauty.WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]/[action]")]
+public class AccountController : BaseController
 {
-    
+    public AccountController(ILogger<AccountController> logger, IMediator mediator) : base(logger, mediator)
+    {
+    }
+
+    [HttpGet]
+    public async Task<ActionResult> GetCurrentAccount()
+    {
+        var result = await _mediator.Send(new CurrentAccountQuery.Request());
+        return Ok(result);
+    }
 }
