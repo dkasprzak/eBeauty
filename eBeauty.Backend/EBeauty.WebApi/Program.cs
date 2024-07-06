b742a76a36dcae9ffab599858491e96d625ccedc
dkasprzak <dominik.kasprzak1408@gmail.com>
2024-07-06 17:52:06 +0200

    cors configurations

--- eBeauty.Backend/EBeauty.WebApi/Program.cs
+++ eBeauty.Backend/EBeauty.WebApi/Program.cs
@@ -48,7 +48,17 @@ builder.Services.AddSwaggerGen(o =>
     });
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
+
+app.UseCors(builder => builder
+    .WithOrigins(app.Configuration.GetValue<string>("WebAppBaseUrl") ?? "")
+    .WithOrigins(app.Configuration.GetSection("AdditionalCorsOrigins").Get<string[]>() ?? new string[0])
+    .WithOrigins((Environment.GetEnvironmentVariable("AdditionalCorsOrigins") ?? "").Split(',').Where(h => !string.IsNullOrEmpty(h)).Select(h => h.Trim()).ToArray())
+    .AllowAnyHeader()
+    .AllowCredentials()
+    .AllowAnyMethod());
 
 app.UseExceptionResultMiddleware();
 
