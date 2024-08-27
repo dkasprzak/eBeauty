866bf2929c50638f0eac4f3e0d7c9c679a73bea3
dkasprzak <dominik.kasprzak1408@gmail.com>
2024-08-27 16:30:19 +0200

    add schedule command

---
+++ eBeauty.Backend/EBeauty.Application.Tests/Logic/BusinessFunctions/DeleteBusinessServicesCommandTests.cs
@@ -0,0 +1,61 @@
+﻿using System.Linq.Expressions;
+using EBeauty.Application.Interfaces;
+using EBeauty.Application.Logic.BusinessFunctions.Commands;
+using EBeauty.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using NSubstitute;
+
+namespace EBeauty.Application.Tests.Logic.BusinessFunctions.Commands
+{
+    public class DeleteBusinessServicesCommandTests
+    {
+        private readonly ICurrentBusinessProvider _businessProvider;
+        private readonly ICurrentAccountProvider _currentAccountProvider;
+        private readonly IApplicationDbContext _applicationDbContext;
+        private readonly DeleteBusinessServicesCommand.Handler _handler;
+
+        public DeleteBusinessServicesCommandTests()
+        {
+            _businessProvider = Substitute.For<ICurrentBusinessProvider>();
+            _currentAccountProvider = Substitute.For<ICurrentAccountProvider>();
+            _applicationDbContext = Substitute.For<IApplicationDbContext>();
+
+            _handler = new DeleteBusinessServicesCommand.Handler(
+                _businessProvider, 
+                _currentAccountProvider, 
+                _applicationDbContext
+            );
+        }
+
+        [Fact]
+        public async Task Handle_Should_Delete_Service_When_Service_Exists()
+        {
+            // Arrange
+            var businessId = 1;
+            var serviceId = 1;
+            var businessTypeId = 2;
+
+            _businessProvider.GetBusinessId().Returns(businessId);
+
+            var service = new Service { Id = serviceId, BusinessId = businessId, BusinessTypeId = businessTypeId, Name = "Test"};
+
+            // Poprawienie konfiguracji mocka, aby używał właściwego predykatu
+            _applicationDbContext.Services
+                .FirstOrDefaultAsync(Arg.Is<Expression<Func<Service, bool>>>(x => x.Compile().Invoke(service)))
+                .Returns(service);
+
+            var request = new DeleteBusinessServicesCommand.Request { ServiceId = serviceId };
+
+            // Act
+            var result = await _handler.Handle(request, CancellationToken.None);
+
+            // Assert
+            Assert.Equal(businessId, result.BusinessId);
+            Assert.Equal(businessTypeId, result.BusinessTypeId);
+
+            // Sprawdzenie, czy Remove i SaveChangesAsync zostały wywołane raz.
+            _applicationDbContext.Services.Received(1).Remove(service);
+            await _applicationDbContext.Received(1).SaveChangesAsync(Arg.Any<CancellationToken>());
+        }
+    }
+}
---
+++ eBeauty.Backend/EBeauty.Application/Logic/ScheduleFunctions/Commands/AddScheduleCommand.cs
@@ -0,0 +1,46 @@
+﻿using EBeauty.Application.Interfaces;
+using EBeauty.Application.Logic.Abstractions;
+using EBeauty.Domain.Entities;
+using MediatR;
+
+namespace EBeauty.Application.Logic.ScheduleFunctions.Commands;
+
+public static class AddScheduleCommand
+{
+    public class Request : IRequest<Result>
+    {
+        public int AccountUserId { get; set; }
+        public DateTimeOffset StartTime { get; set; }
+        public DateTimeOffset EndTime { get; set; }
+    }
+    
+    public class Result
+    {
+        public required int ScheduleId { get; set; }
+    }
+    
+    public class Handler : BaseCommandHandler, IRequestHandler<Request, Result>
+    {
+        public Handler(ICurrentAccountProvider currentAccountProvider, IApplicationDbContext applicationDbContext) : base(currentAccountProvider, applicationDbContext)
+        {
+        }
+
+        public async Task<Result> Handle(Request request, CancellationToken cancellationToken)
+        {
+            var schedule = new Schedule
+            {
+                AccountUserId = request.AccountUserId,
+                StartTime = request.StartTime,
+                EndTime = request.EndTime
+            };
+
+            _applicationDbContext.Schedules.Add(schedule);
+            await _applicationDbContext.SaveChangesAsync();
+
+            return new Result()
+            {
+                ScheduleId = schedule.Id
+            };
+        }
+    }
+}
